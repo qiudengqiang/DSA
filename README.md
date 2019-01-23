5184fd115546537eb81340be38b91c342f68546e
apple <Apple@Knife-Macbook.local>
2019-01-23 18:54:52 +0800

    ADD - longest common prefix

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # leetcode
-刷题
+========
+LeetCode Problems' Solutions
+
+### LeetCode Algorithm
+(Notes: "&hearts;" means you need to buy a book from Leetcode)
+
+
+| # | Title | Solution | Difficulty |
+|---| ----- | -------- | ---------- |
+|1|[Two Sum](https://oj.leetcode.com/problems/two-sum/)| [C++](./algorithms/cpp/twoSum/twoSum.c)|Easy|
+|14|[Longest Common Prefix](https://oj.leetcode.com/problems/longest-common-prefix/)| [C++](./algorithms/cpp/longestCommonPrefix/longestCommonPrefix.c)|Easy|
+
+### LeetCode Database
+
+### LeetCode Shell
+
---
+++ algorithms/cpp/longestCommonPrefix/longestCommonPrefix.c
@@ -0,0 +1,42 @@
+//
+//  LongestCommonPrefix.c
+//  Exam
+//
+//  Created by Apple on 2019/1/22.
+//  Copyright © 2019 apple. All rights reserved.
+//
+
+#include <stdio.h>
+#include <string.h>
+/**
+ 编写一个函数来查找字符串数组中的最长公共前缀。
+ 如果不存在公共前缀，返回空字符串 ""。
+ 
+ 示例 1:
+ 输入: ["flower","flow","flight"]
+ 输出: "fl"
+ 
+ 示例 2:
+ 输入: ["dog","racecar","car"]
+ 输出: ""
+ 解释: 输入不存在公共前缀。
+ */
+
+char* longestCommonPrefix(char** strs, int strsSize) {
+    //要明白是比较数组中所有字符的公共最大前缀
+    //思路:把数组看做一个二维数组，逐个字符进行比较
+    char* str = strs[0];
+    int i,j;
+    
+    if(strsSize == 0) return "";
+    
+    for(i=1;i<strsSize;i++){
+        j=0;
+        while(str[j] && strs[i][j] && str[j]==strs[i][j]){
+            j++;
+        }
+        str[j]='\0';
+    }
+    return str;
+}
+
