b359c32f1fd8bb44faf4959c690dced10492e90c
techbird <qiudengqiang@gmail.com>
2020-07-15 10:51:13 +0800

    自定义LinkedList

--- algorithm/src/me/techbird/leetcode/test/Main.java
+++ algorithm/src/me/techbird/leetcode/test/Main.java
@@ -1,14 +1,22 @@
 package me.techbird.leetcode.test;
 
 import me.techbird.leetcode.practice.ArrayList;
+import me.techbird.leetcode.practice.LinkedList;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Person> list = new ArrayList<>();
-        list.add(new Person("aa", 11));
-        for (int i = 0; i < 20; i++) {
-            list.add(new Person("a"+i, i));
-            System.out.println(list);
-        }
+        LinkedList<Integer> list = new LinkedList<>();
+        list.add(20);
+        list.add(0, 10);
+        list.add(30);
+        list.add(list.size(), 40);
+        list.remove(1);
+        System.out.println(list);
+//        ArrayList<Person> list = new ArrayList<>();
+//        list.add(new Person("aa", 11));
+//        for (int i = 0; i < 20; i++) {
+//            list.add(new Person("a"+i, i));
+//            System.out.println(list);
+//        }
     }
 }
