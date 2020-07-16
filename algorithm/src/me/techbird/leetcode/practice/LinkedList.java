c350e10b60092d6da8cde84b29aaa644c5898712
DESKTOP-B15PMER\QDQ <qiudengqiang@gmail.com>
2020-07-16 10:32:35 +0800

    reback LinkedList code

--- algorithm/src/me/techbird/leetcode/practice/LinkedList.java
+++ algorithm/src/me/techbird/leetcode/practice/LinkedList.java
@@ -21,31 +21,96 @@ public class LinkedList<E> extends AbstractList<E> {
 
     @Override
     public void add(int index, E element) {
+        rangeCheckForAdd(index);
+        if (index == 0) {
+            first = new Node<>(element, first);
+        } else {
+            Node<E> prev = node(index - 1);
+            prev.next = new Node<E>(element, prev.next);
+        }
 
+        size++;
     }
 
     @Override
     public E get(int index) {
-        return null;
+        return node(index).element;
     }
 
     @Override
     public E set(int index, E element) {
-        return null;
+        Node<E> node = node(index);
+        E old = node.element;
+        node.element = element;
+        return old;
     }
 
     @Override
     public E remove(int index) {
-        return null;
+        rangeCheck(index);
+        Node<E> node = first;
+        if (index == 0) {
+            first = first.next;
+        } else {
+            Node<E> prev = node(index - 1);
+            node = prev.next;
+            prev.next = node.next;
+        }
+        size--;
+        return node.element;
     }
 
     @Override
     public boolean contains(E element) {
-        return false;
+        return indexOf(element) == ELEMENT_NOT_FOUND;
     }
 
     @Override
     public int indexOf(E element) {
-        return 0;
+        if (element == null) {
+            Node<E> node = first;
+            for (int i = 0; i < size; i++) {
+                if (null == node.element) return i;
+                node = node.next;
+            }
+        } else {
+            Node<E> node = first;
+            for (int i = 0; i < size; i++) {
+                if (element.equals(node.element)) return i;
+                node = node.next;
+            }
+        }
+        return ELEMENT_NOT_FOUND;
+    }
+
+    /**
+     * 查找链表中的Index节点
+     *
+     * @param index
+     * @return
+     */
+    private Node<E> node(int index) {
+        rangeCheck(index);
+        Node<E> node = first;
+        for (int i = 0; i < index; i++) {
+            node = node.next;
+        }
+        return node;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Size=").append(size).append(",[");
+        Node<E> node = first;
+        for (int i = 0; i < size; i++) {
+            if (i != 0) {
+                sb.append(",");
+            }
+            sb.append(node.element);
+            node = node.next;
+        }
+        sb.append("]");
+        return sb.toString();
     }
 }
