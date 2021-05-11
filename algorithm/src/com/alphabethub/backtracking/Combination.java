52682f56a91a07f71c3e61bc966f9ee38a7f2304
techbird <qiudengqiang@gmail.com>
2021-05-11 11:11:39 +0800

    ADD - 回溯之组合问题

---
+++ algorithm/src/com/alphabethub/backtracking/Combination.java
@@ -0,0 +1,35 @@
+package com.alphabethub.backtracking;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * 输入两个数字n,k，算法输出[1..n]中k个数字的所有组合
+ */
+public class Combination {
+
+    public static void main(String[] args) {
+        List<List<Integer>> result = new Combination().combination(4, 2);
+        System.out.println(result);
+    }
+
+    List<List<Integer>> res = new LinkedList<>();
+
+    public List<List<Integer>> combination(int n, int k) {
+        LinkedList<Integer> track = new LinkedList<>();
+        backtrack(n, k, 1, track);
+        return res;
+    }
+
+    void backtrack(int n, int k, int start, LinkedList<Integer> track) {
+        if (track.size() == k) {
+            res.add(new LinkedList<>(track));
+            return;
+        }
+        for (int i = start; i <= n; i++) {
+            track.add(i);
+            backtrack(n, k, i + 1, track);
+            track.removeLast();
+        }
+    }
+}
