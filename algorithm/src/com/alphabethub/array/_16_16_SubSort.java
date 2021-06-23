42c9171abb60d524ebe5b9a32632315723d6818d
techbird <qiudengqiang@gmail.com>
2021-06-23 14:56:25 +0800

    MOD - 修复判断的bug

--- algorithm/src/com/alphabethub/array/_16_16_SubSort.java
+++ algorithm/src/com/alphabethub/array/_16_16_SubSort.java
@@ -12,16 +12,17 @@ public class _16_16_SubSort {
         System.out.println(Arrays.toString(subSort(nums)));
     }
 
-    public static int[] subSort(int[] nums) {
-        if (nums.length == 0) return new int[]{-1, -1};
+    public static int[] subSort(int[] array) {
+        if (array.length == 0) return new int[]{-1, -1};
 
         //从左到右扫描逆序对，正序：逐渐变大
-        int max = nums[0];
+        int max = array[0];
         //用于记录最右逆序对位置
         int r = -1;
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i] > max) {
-                max = nums[i];
+        for (int i = 1; i < array.length; i++) {
+            //判断一定要使用>=
+            if (array[i] >= max) {
+                max = array[i];
             } else {
                 r = i;
             }
@@ -31,12 +32,13 @@ public class _16_16_SubSort {
         if (r == -1) return new int[]{-1, -1};
 
         //从右往左扫描逆序对，正序：逐渐变小
-        int min = nums[nums.length - 1];
+        int min = array[array.length - 1];
         //用于记录最左的逆序对位置
         int l = -1;
-        for (int i = nums.length - 2; i >= 0; i--) {
-            if (nums[i] < min) {
-                min = nums[i];
+        for (int i = array.length - 2; i >= 0; i--) {
+            //判断一定要使用<=
+            if (array[i] <= min) {
+                min = array[i];
             } else {
                 l = i;
             }
