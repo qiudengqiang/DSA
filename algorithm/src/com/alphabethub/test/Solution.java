fe44184eba9c7ef5761dc04788ea762a51ccb7f4
qiudengqiang <qiudengqiang@gmail.com>
2021-10-24 10:28:01 +0800

    string problem

--- algorithm/src/com/alphabethub/test/Solution.java
+++ algorithm/src/com/alphabethub/test/Solution.java
@@ -3,7 +3,11 @@ package com.alphabethub.test;
 public class Solution {
 
     public static void main(String[] args) {
-
+        String s1 = "12";
+        String s2 = new String("12");
+        String s3 = "1"+"2";
+        System.out.println(s1 == s2);
+        System.out.println(s1 == s3);
     }
 
 }
